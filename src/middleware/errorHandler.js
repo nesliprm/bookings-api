bfecaa4f22e67f7dd063adebdb2c94640a3d3dea
Nesli <nesliparma@gmail.com>
2025-07-27 17:53:20 +0200

    tweak error message

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,11 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
-  res.status(500).json({ message: "Something went wrong!" });
+  res
+    .status(500)
+    .json({
+      message:
+        "An error occurred on the server, please double-check your request!",
+    });
 };
 
 export default errorHandler;
