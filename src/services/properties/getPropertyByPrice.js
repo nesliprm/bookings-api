614239768a3dd24bc827b579db59faf441d2a074
Nesli <nesliparma@gmail.com>
2025-08-11 18:08:34 +0200

    Update getPropertyByPrice to filter by exact price

--- src/services/properties/getPropertyByPrice.js
+++ src/services/properties/getPropertyByPrice.js
@@ -1,11 +1,8 @@
 import prisma from "../../lib/prisma.js";
 
-const getPropertyByPrice = async (maxPrice) => {
-  // Matching exact price is a bit strange,
-  // so I decided to get properties with equal or less than the queried price.
-
+const getPropertyByPrice = async (pricePerNight) => {
   return prisma.property.findMany({
-    where: { pricePerNight: { lte: Number(maxPrice) } },
+    where: { pricePerNight },
   });
 };
 
